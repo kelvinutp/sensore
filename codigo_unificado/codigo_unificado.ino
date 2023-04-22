76741ab0752cca336271949efc556459ac6d928e
Kelvin Kung <48689965+kelvinutp@users.noreply.github.com>
2023-04-21 23:36:05 -0500

    codigo de sensores unificado
    Se encuentra el codigo para sensores
    de corriente y voltaje
    de humedad
    de NPK
    de TDS

--- codigo_unificado/codigo_unificado.ino
+++ codigo_unificado/codigo_unificado.ino
@@ -37,23 +37,23 @@ float temperature = 25;       // current temperature for compensation
 
 void setup() {
   // declaracion de funciones de pines
-  
+
   //sensor de voltaje y corriente
   pinMode(a, INPUT);
   pinMode(v, INPUT);
-  
+
   //sensor NPK
   pinMode(txPin, OUTPUT);
   pinMode(rxPin, INPUT);
   connection.begin(9600);
-  
+
   //sensor DHT11
   pinMode(le, INPUT);
-  
-  //sensor TDS
-  pinMode(TdsSensorPin,INPUT);
 
-  
+  //sensor TDS
+  pinMode(TdsSensorPin, INPUT);
+
+
   //inicio de comunicacion serial
   Serial.begin(9600);
   Serial.println("Inicio de programa");
@@ -75,8 +75,38 @@ void loop() {
   delay(250);
 
   //lectura de humedad
-  int hu=humedad();
-  
+  int hu = humedad();
+
+  //TDS
+  static unsigned long analogSampleTimepoint = millis();
+  if (millis() - analogSampleTimepoint > 40U) { //every 40 milliseconds,read the analog value from the ADC
+    analogSampleTimepoint = millis();
+    analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);    //read the analog value and store into the buffer
+    analogBufferIndex++;
+    if (analogBufferIndex == SCOUNT) {
+      analogBufferIndex = 0;
+    }
+  }
+
+  static unsigned long printTimepoint = millis();
+  if (millis() - printTimepoint > 800U) {
+    printTimepoint = millis();
+    for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++) {
+      analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+
+      // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+      averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREF / 4096.0;
+
+      //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+      float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);
+      //temperature compensation
+      float compensationVoltage = averageVoltage / compensationCoefficient;
+
+      //convert voltage value to tds value
+      tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * 0.5;
+    }
+  }
+
   //impresiones en monitor serial
 
   //voltaje y corriente
@@ -101,7 +131,13 @@ void loop() {
   Serial.print(hu);
   Serial.println("%");
 
-  
+  //TDS
+  Serial.print("voltage:");
+  Serial.print(averageVoltage, 2);
+  Serial.print("V   ");
+  Serial.print("TDS Value:");
+  Serial.print(tdsValue, 0);
+  Serial.println("ppm");
   delay(100);
 
 
@@ -168,8 +204,31 @@ byte potassium() {
   return values[4];
 }
 
-int humedad(){
+int humedad() {
   //valor en porcentaje
-  int ac=map(analogRead(le),0,1023,0,100);
+  int ac = map(analogRead(le), 0, 1023, 0, 100);
   return ac;
+}
+
+int getMedianNum(int bArray[], int iFilterLen){
+  int bTab[iFilterLen];
+  for (byte i = 0; i<iFilterLen; i++)
+  bTab[i] = bArray[i];
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++) {
+    for (i = 0; i < iFilterLen - j - 1; i++) {
+      if (bTab[i] > bTab[i + 1]) {
+        bTemp = bTab[i];
+        bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+      }
+    }
+  }
+  if ((iFilterLen & 1) > 0){
+    bTemp = bTab[(iFilterLen - 1) / 2];
+  }
+  else {
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+  }
+  return bTemp;
 }
