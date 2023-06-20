467f28c40b14cefa2fd1502b170ac09968285aca
Juancho06 <100179037+Juancho06@users.noreply.github.com>
2023-06-19 22:14:34 -0500

    Add files via upload
    codigos de los sensores de pH calibrados
    Un codigo por cada sensor marcado

---
+++ Sensores pH/Sensor_2/Sensor_2.ino
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+
+int pHSense = A0;
+int samples = 10;
+float adc_resolution = 1024.0;
+
+void setup()
+{
+    Serial.begin(9600);
+    delay(100);
+    Serial.println("cimpleo pH Sense");
+}
+
+float ph (float voltage) {
+    return (-5.5388)*(voltage)+20.85;;
+}
+
+void loop()
+{
+    int measurings=0;
+
+    for (int i = 0; i < samples; i++)
+    {
+        measurings += analogRead(pHSense);
+        delay(10);
+
+    }
+
+    float voltage = 5 / adc_resolution * measurings/samples;
+    Serial.print(("Voltage= "));
+    Serial.print((voltage));
+    Serial.print(("   "));
+    Serial.print("pH= ");
+    Serial.println(ph(voltage));
+    delay(3000);
+}
